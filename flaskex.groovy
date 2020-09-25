da2be38d7237f9a3f8d0888907903d2e5950d3f2
Vladyslav Bielov <68798347+vladbielov@users.noreply.github.com>
2020-09-25 01:56:43 -0500

    Update flaskex.groovy

--- flaskex.groovy
+++ flaskex.groovy
@@ -24,6 +24,9 @@ node {
         stage("pip update") {
             sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } yum install -y python36u python36u-libs python36u-devel python36u-pip -y"
         }
+        stage("default Python3.6") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } alias python="/usr/bin/python3.6"
+        }
         stage("pip update") {
             sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } pip install --upgrade pip"
         }         
