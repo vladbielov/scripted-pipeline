8d2a8d2d78535725036d23b185c1c90ce6b0299a
Vladyslav Bielov <68798347+vladbielov@users.noreply.github.com>
2020-09-25 12:07:31 -0500

    Update pet_11.groovy

--- pet_11.groovy
+++ pet_11.groovy
@@ -6,26 +6,13 @@ properties([
 
 node {
     withCredentials([sshUserPrivateKey(credentialsId: 'jenkins-master', keyFileVariable: 'SSHKEY', passphraseVariable: '', usernameVariable: 'SSHUSERNAME')]) {
-        stage("yum update") {
-            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } yum update -y "
+        stages {
+            stage ('Install yum update'){
+                steps {
+                    sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } "
+                    yum update -y 
+                }
+            }
         }
-        stage("Install git") {
-            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } yum install git -y"
-        }
-        stage("Install java") {
-            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } yum install java-1.8.0-openjdk -y"
-        }
-        stage("Install maven") {
-            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } yum install maven -y"
-        }
-        stage("clone spring-petclinic.git") {
-            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } git clone https://github.com/spring-projects/spring-petclinic.git"
-        }
-        stage("install mvnw package") {
-            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } ~/spring-petclinic/mvnw package"
-        }
-        stage("run jar file") {
-            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } java -jar  ~/spring-petclinic/target/*.jar"
-        } 
     }
 }
