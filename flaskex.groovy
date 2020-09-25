2aa5868b92131ef9b7f8253730840b769f311a57
Vladyslav Bielov <68798347+vladbielov@users.noreply.github.com>
2020-09-24 20:32:10 -0500

    Update flaskex.groovy

--- flaskex.groovy
+++ flaskex.groovy
@@ -18,17 +18,14 @@ node {
         stage("Install python") {
             sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } yum install python-pip -y"
         }
-        stage("Install update pip") {
-            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } pip install --update pip"
-        }
-        stage("Install clone Flaskex") {
+                stage("Install clone Flaskex") {
             sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } git clone https://github.com/anfederico/Flaskex"
         }
         stage("pip requirements") {
             sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } pip install -r /root/Flaskex/requirements.tx"
         }
         stage("pip update") {
-            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } pip install -U pip"
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } pip install --upgrade pip"
         }
         stage("run app") {
             sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } python /root/Flaskex/app.py"
