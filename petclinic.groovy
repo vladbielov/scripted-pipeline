1e129e7d674c18cd5c5147f65e6044ff4614777c
Vladyslav Bielov <68798347+vladbielov@users.noreply.github.com>
2020-09-25 10:31:24 -0500

    Update petclinic.groovy

--- petclinic.groovy
+++ petclinic.groovy
@@ -12,32 +12,21 @@ node {
         stage("Install git") {
             sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } yum install git -y"
         }
-        stage("Install epel-release") {
-            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } yum install epel-release -y"
+        stage("Install java") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } yum install java-1.8.0-openjdk -y"
         }
-        stage("Install python") {
-            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } yum install python-pip -y"
+        stage("Install maven") {
+            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } yum install maven -y"
         }
-        stage("Install repo") {
-            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } yum install -y https://repo.ius.io/ius-release-el7.rpm"
-        }
-        stage("pip update") {
-            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } yum install -y python36u python36u-libs python36u-devel python36u-pip -y"
-        }
-        stage("default Python3.6") {
-            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } alias python=python3"
-        }
-        stage("pip update") {
-            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } pip install --upgrade pip"
-        }         
-        stage("Install clone Flaskex") {
-            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } git clone https://github.com/anfederico/Flaskex"
-        }
-        stage("pip requirements") {
-            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } pip install -r ./Flaskex/requirements.txt"
-        }       
-        stage("run app") {
-            sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE } python ./Flaskex/app.py"
+        stage("install spring-petclinic") {
+            steps {
+                sh "ssh -o StrictHostKeyChecking=no -i $SSHKEY $SSHUSERNAME@${ params.SSHNODE }" 
+                git clone "https://github.com/spring-projects/spring-petclinic.git"
+                cd spring-petclinic
+                ./mvnw package
+                java -jar target/spring-petclinic-2.3.0.BUILD-SNAPSHOT.jar
+                
+            }
         }
     }
 }
